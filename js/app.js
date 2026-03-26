9613e37d37b2db416ea5c7e2962a936bdb1f7415
Hasibul Hoque Probal <hoqueefrom@gmail.com>
2026-03-26 12:16:55 +0600

    Clear call history functionality

--- js/app.js
+++ js/app.js
@@ -70,3 +70,15 @@ function addCallHistory(title, number) {
     historyList.appendChild(newEntry);
 }
 
+// Clear History Feature
+function clearHistory() {
+    var historyList = document.getElementById("call-history");
+    for (let i = historyList.children.length - 1; i >= 0; i--) {
+        let child = historyList.children[i];
+        if (child.classList.contains("call-entry")) {
+            historyList.removeChild(child);
+        }
+    }
+    alert("Call history cleared.");
+}
+
